3e094aca948b2e70ca751a4c52a87a6274a390d1
minisoftwr <chelluuniii2@gmail.com>
2026-09-02 22:08:29 +0100

    Adding the cosine similiarity and TF-IDF vectorizer

--- recommender.py
+++ recommender.py
@@ -2,7 +2,7 @@ import pandas as pd
 import ast 
 from pathlib import Path
 
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 BASE_DIR = Path(__file__).parent
@@ -26,11 +26,13 @@ def load_books():
     books["mood"] = books["mood"].apply(normalization)
 
     return books
-def features(books):
+def create_features(books):
     # Combine the genre list,mood list and the book title into one text so that TF-IDF can understand
     books["features"] = books.apply(
         lambda book:
-        " ".join(book["genre"]) + " " + " ".join(book["mood"]) + " " + str(book["title"]),axis=1
+        " ".join(book["genre"]) + " " +
+        " ".join(book["mood"]) + " " + 
+        str(book["title"]),axis=1
     )# Apply this to every book
 
     # Return the dataframe with new "features" column
@@ -41,15 +43,30 @@ def get_recommendations(genre, mood):
 
     if not genre or not mood:
         return []
+    # Create The feature column
+    books =create_features(books)
+
+    # Applying the Tfid Vectorizer
+    tfidf = TfidfVectorizer()
+    tfidf_matrix = tfidf.fit_transform(books["features"])
+    # Create user preference
+    user_matrix = tfidf.transform([f"{genre} {mood}"])
     
-    # find books matching the mood
+    # Cosine similarity
+    similarity = cosine_similarity(user_matrix,tfidf_matrix).flatten()
+    books["similarity_score"] = similarity
+
+    # Find books matching the mood
     genre_books = books[books["genre"].apply(lambda genres: genre in genres)]
     mood_books = genre_books[genre_books["mood"].apply(lambda moods: mood in moods)]
 
+    if mood_books.empty:
+        return []
+    
     mood_books = mood_books.dropna(subset=["average_rating"])
 
-    # return the best-rated books
-    recommendations = mood_books.sort_values(by="average_rating",ascending=False).head(10)
+    # Return the best-rated books
+    recommendations = mood_books.sort_values(by=["similarity_score","average_rating"],ascending=[False,False]).head(17)
 
     result = recommendations.to_dict(orient="records")
 
